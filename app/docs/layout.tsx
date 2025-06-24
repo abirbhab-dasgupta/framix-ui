d3ffd9318fbd0d512cc6e240cd6678bc40d6914d
Abirbhab <codewithad24@gmail.com>
2025-06-25 02:07:16 +0530

    fix: update package dependencies and improve layout metadata

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -2,8 +2,15 @@ import { source } from "@/lib/source";
 import { DocsLayout } from "fumadocs-ui/layouts/notebook";
 import type { ReactNode } from "react";
 import { baseOptions } from "../layout.config";
+import type { Metadata } from "next";
 
-
+export const metadata: Metadata = {
+  title: {
+    template:
+      "%s |framixui - UI Components for new age developers",
+    default: "framixui - UI Components to build beautiful websites",
+  },
+};
 
 
 export default function Layout({ children }: { children: ReactNode }) {
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,20 +1,19 @@
 import GitHubIcon from "@/components/icons/github";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
-import { Flame, Github } from "lucide-react";
+import {Frame } from "lucide-react";
 
 
 
-export const baseOptions = {
+export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <div className="flex items-center">
-        <Flame className="mr-2 h-5 w-5 text-blue-600" />
+        <Frame className="mr-2 h-5 w-5 text-green-600" />
         <span className="hidden md:inline-flex items-center text-lg font-bold tracking-tight text-black dark:text-white">
           FramixUI
         </span>
       </div>
     ),
-    // You can add search, theme toggle, etc. here if supported
   },
   links: [
     {
@@ -23,16 +22,8 @@ export const baseOptions = {
     },
 
     {
-      text: "Pricing",
-      url: "/pricing",
-    },
-
-    {
       type: "custom",
-      children: <GitHubIcon/>,
+      children: <GitHubIcon />,
     },
-
-   
   ],
-  // ...other options
 };

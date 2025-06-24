8ddc0494852ec7639748320c58f097eb6f4edc50
Abirbhab <codewithad24@gmail.com>
2025-06-25 02:12:14 +0530

    fix: change Frame icon color changed

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -8,7 +8,7 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <div className="flex items-center">
-        <Frame className="mr-2 h-5 w-5 text-green-600" />
+        <Frame className="mr-2 h-5 w-5 text-blue-600" />
         <span className="hidden md:inline-flex items-center text-lg font-bold tracking-tight text-black dark:text-white">
           FramixUI
         </span>
